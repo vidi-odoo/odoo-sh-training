c307d5e5d4567df508ec8061a777809f64831687
Viet Dinh <vidi@odoo.com>
2022-06-13 13:46:16 -0400

    [IMP] Add demo data

--- spacecrew/models/models.py
+++ spacecrew/models/models.py
@@ -6,5 +6,6 @@ class Spaceship(models.Model):
     _name = 'spacecrew.spaceship'
     _description = 'Spaceship'
 
+    name = fields.Char()
     active = fields.Boolean(string='Active', default=True)
     manufacturer = fields.Char()
