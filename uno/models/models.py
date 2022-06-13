79f955bde0188deb7135315c6b556e4a21b190e2
Viet Dinh <vidi@odoo.com>
2022-06-13 13:19:51 -0400

    [FIX] Typos

--- uno/models/models.py
+++ uno/models/models.py
@@ -5,3 +5,5 @@ from odoo import models, fields, api
 class Uno(models.Model):
     _name = 'uno.uno'
     _description = 'Dummy model'
+
+    flag = fields.Boolean(string='Some random flag', default=True)
