fffcd9ae267f59015de6272c96b8ffa15927340f
Viet Dinh <vidi@odoo.com>
2022-06-13 12:59:38 -0400

    [IMP] Add fields to models

--- volunteers/models/models.py
+++ volunteers/models/models.py
@@ -6,3 +6,15 @@ class Task(models.Model):
     _name = 'volunteers.task'
     _description = 'Voluteer Tasks'
 
+    name = fields.Char(string='Name')
+    description = fields.Text(string='Description')
+    start_time = fields.DateTime(string='Start time')
+    stop_time = fields.DateTime(string='Stop time')
+    times_repeated = fields.Integer(string='Times repeated')
+    frequency = fields.Selection(string='Frequency', selection=[
+        ('hour', 'Hour'),
+        ('day', 'Day'),
+        ('week', 'Week'),
+        ('month', 'Month'),
+        ('year', 'Year')
+    ])
