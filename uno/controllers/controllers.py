aa3d28f795c914e8b5935f9b704900beeb38db66
Viet Dinh <vidi@odoo.com>
2022-06-13 10:00:00 -0400

    Scaffold module

---
+++ uno/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class ./uno(http.Controller):
+#     @http.route('/./uno/./uno', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/./uno/./uno/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('./uno.listing', {
+#             'root': '/./uno/./uno',
+#             'objects': http.request.env['./uno../uno'].search([]),
+#         })
+
+#     @http.route('/./uno/./uno/objects/<model("./uno../uno"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('./uno.object', {
+#             'object': obj
+#         })
