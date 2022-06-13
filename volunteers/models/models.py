8d804db8731d7b64edaa716ca9add44343427fc7
Viet Dinh <vidi@odoo.com>
2022-06-13 11:24:19 -0400

    [IMP] Add new models

--- volunteers/models/models.py
+++ volunteers/models/models.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class Task(models.Model):
+    _name = 'volunteers.task'
+    _description = 'Voluteer Tasks'
 
-# class volunteers(models.Model):
-#     _name = 'volunteers.volunteers'
-#     _description = 'volunteers.volunteers'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
